e98689d1ff69c528b57eabd77ddcfe688f83e6c2
Philip Ajayi <149468515+Philip-Ajayi@users.noreply.github.com>
2025-06-04 17:02:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
