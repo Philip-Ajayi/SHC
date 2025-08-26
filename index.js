42f700fb316f7d0e3c1cd5e1c145d2eef552f4bc
Philip Ajayi <149468515+Philip-Ajayi@users.noreply.github.com>
2025-08-26 18:33:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
   address: String,
   location: {
     type: String,
+    required: true,
     enum: ['New-York', 'Indiana', 'Maryland'] // ✅ only allow these values
   },
   year: {
@@ -364,4 +365,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
